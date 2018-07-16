3dd497ef462263a44d2511af1d2b509eee8e82ab
koechkevin <koechkevin92@gmail.com>
2018-07-16 17:33:29 +0300

    user can view details of an entry

--- java.js
+++ java.js
@@ -33,9 +33,20 @@ function validateLogin() {
     }   
     
 } 
-
-
-
+var modal = document.getElementById('myModal');
+var btn = document.getElementById("myBtn");
+var span = document.getElementsByClassName("close")[0];
+btn.onclick = function() {
+    modal.style.display ="block";
+}
+span.onclick = function() {
+    modal.style.display = "none";
+}
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
 
 
 
