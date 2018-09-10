511d6c630644f9e45042fab967f7fb5e995e5b3f
KEVIN KOECH <koech@KEVINs-MacBook-Pro.local>
2018-09-10 07:49:48 +0300

    attempt test

--- static/index.js
+++ static/index.js
@@ -4,13 +4,11 @@ document.addEventListener("DOMContentLoaded", ()=>{
     fetch(route+"/", {
         method:"GET",
         headers:{"Content-Type":"application/json"}})
-        .then((response) => response.json())
-        .then((data)=>{
-            document.getElementById("output").innerHTML = data["message"];
-        })
+        .then(response => response.json())
+        .then(data=>data["message"])
+        .then((message)=>document.getElementById("output").innerHTML = message)
         .catch((err) => console.log(err))
 }, false);
-
 
 document.getElementById("logo").addEventListener("click",
 function home(){
--- test/index.test.js
+++ test/index.test.js
@@ -1,25 +1,11 @@
 describe("message on landing page",() => {
-    Mock = jest.spyOn(global, 'fetch');
-    Mock.mockImplementation(() => Promise.resolve({
-    json: () => Promise.resolve({"status": "success", "Message": "welcome to my diary. This is it!"})
-    }))
-    document.body.innerHTML = `
-        <div class="topnav">
-        <img class="view" id = "logo" src="/static/myDiary.png" alt="logo" />
-        </div>
-        <div class = "welcome">
-        <h1>
-            <font color="white">
-            <div id="output">welcome to my diary. This is it!</div>
-            </font>
-        </h1>
-        </div>
-        `;
-    spyMock = jest.spyOn(window.location,"assign");
-    spyMock.mockImplementation();    
-    require("../static/index.js")
-    it("Ensures index page is loaded", async()=>{
-        window.document.dispatchEvent(new Event("DOMContentLoaded", {}));
+   document.body.innerHTML =`
+   <img class="view" id = "logo" src="../static/myDiary.png" alt="logo" /> 
+   <div id="output"></div>
+   `;
+   mock = jest.spyOn(global,"fetch");
+    require("../static/index.js");
+    it("Ensures index page is loaded", async()=>{    
+        
     })
-    expect(document.getElementById('output').innerHTML).toBe("welcome to my diary. This is it!");
 })
